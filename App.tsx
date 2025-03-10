8cefc652dd15aa37356ccd9558e96b3eb01e2e4b
armitakatarapu <akatarapu@gmail.com>
2025-03-09 19:25:40 -0500

    OG File

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-/**
- * Sample React Native App
+/*** Sample React Native App
  * https://github.com/facebook/react-native
  *
  * @format
@@ -64,7 +63,7 @@ function App(): React.JSX.Element {
   /*
    * To keep the template simple and small we're adding padding to prevent view
    * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
+   * For bigger apps the reccomendation is to use react-native-safe-area-context:
    * https://github.com/AppAndFlow/react-native-safe-area-context
    *
    * You can read more about it here:
